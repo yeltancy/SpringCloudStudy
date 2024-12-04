df13a463567240ac98f62c9c2da26e5f0e07ba65
yeltancy <2522087570@qq.com>
2024-12-04 23:38:12 +0800

    Eureka学习

--- service-borrow/src/main/java/org/example/config/BeanConfig.java
+++ service-borrow/src/main/java/org/example/config/BeanConfig.java
@@ -1,11 +1,13 @@
 package org.example.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@LoadBalancerClient(name = "service-user", configuration = LoadBalancerConfig.class)
 public class BeanConfig {
     @Bean
     @LoadBalanced
---
+++ service-borrow/src/main/java/org/example/config/LoadBalancerConfig.java
@@ -0,0 +1,18 @@
+package org.example.config;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+//随机负载均衡器配置类
+public class LoadBalancerConfig {
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(Environment environment, LoadBalancerClientFactory loadBalancerClientFactory){
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class), name);
+    }
+}
